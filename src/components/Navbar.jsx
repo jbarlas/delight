836cecd74c603f2ac1fb14d1bbd610e28705b2c8
jbarlas <justin_barlas@brown.edu>
2022-12-27 22:13:15 -0500

    just added folders for components and views

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>Navbar</>
+  )
+}
+
+const styles = StyleSheet.create({
+    // style shit goes in here
+})
---
+++ src/views/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <>Profile</>
+  )
+}
+
+const styles = StyleSheet.create({
+    // style shit goes in here
+})
