733cc80f2bd0f217eb08dce8aa55545bcefb859c
jbarlas <justin_barlas@brown.edu>
2023-01-03 16:55:28 -0500

    merges

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { React } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Login from "./views/Login";
-import Profile from "./views/Profile"
-import Settings from "./views/Settings"
+import Profile from "./views/Profile";
+import Settings from "./views/Settings";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown:false}}>
+    <>
+      <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Profile" component={Profile} initialParams={{text: "testing"}}/>
-        <Tab.Screen name="Settings" component={Settings}/>
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
+    </>
   );
 }
